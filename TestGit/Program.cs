e3b5d3f4d3f1f2fbdd888a225bb4a613eafda8fc
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-09-12 14:07:49 +0200

    New line added with very important information.

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Första testprogrammet jag skickar upp till git!");
+
+            Console.WriteLine("Lägger till en rad för att har något att köra en push till git!");
         }
     }
 }
