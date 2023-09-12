6e6576d011ea1aeec1e800af4a81af7bf698d8b5
samue <samue@SL_MUSIK>
2023-09-12 14:01:02 +0200

    Add project files.

---
+++ TestGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Första testprogrammet jag skickar upp till git!");
+        }
+    }
+}
